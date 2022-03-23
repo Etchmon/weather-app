828b75e4125786eb9628d2bab4f1741ac3e5b5a5
James Etchells <etch.rush@gmail.com>
2022-03-23 14:08:17 -0700

    Add pass input value through api calls and return data based on input

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const Handler = (() => {
-    async function oneCallApi() {
-        const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=880ffa59f6aeed6d569e7450459fec7e');
+    async function oneCallApi(lat, long) {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,daily&appid=880ffa59f6aeed6d569e7450459fec7e`);
 
         const oneCallData = await response.json();
         console.log(oneCallData);
+
+        return oneCallData;
     }
 
-    async function getCoordinates() {
-        const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=880ffa59f6aeed6d569e7450459fec7e');
+    async function getCoordinates(city) {
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=880ffa59f6aeed6d569e7450459fec7e`);
 
         const coordinateData = await response.json();
         console.log(coordinateData);
+
+        return coordinateData;
     }
 
     console.log(navigator.geolocation.getCurrentPosition(position => {
@@ -18,8 +22,7 @@ const Handler = (() => {
         console.log(latitude, longitude)
     }));
 
-    oneCallApi();
-    getCoordinates();
+    return { getCoordinates, oneCallApi }
 })();
 
 const Display = (() => {
@@ -37,10 +40,19 @@ const Display = (() => {
             placeholder: 'city name...'
         });
 
-        function getInput() {
-            console.log(input.value);
-            return input.value;
-        }
+        async function getInput() {
+            const data = await Handler.getCoordinates(input.value);
+            console.log(data);
+            const lat = data[0].lat.toFixed(2);
+            const long = data[0].lon.toFixed(2);
+
+            console.log(lat, long);
+
+            const dataApi = await Handler.oneCallApi(lat, long);
+
+            console.log(dataApi);
+
+        };
 
         container.appendChild(input);
         container.appendChild(btn);
