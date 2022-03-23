b09900625fed3514cd61c1d5147a981ffc996b5a
James Etchells <etch.rush@gmail.com>
2022-03-23 11:44:38 -0700

    Add async geolocator call to weather api Add current geolocation function

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-async function oneCallApi() {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=880ffa59f6aeed6d569e7450459fec7e');
+const Handler = (() => {
+    async function oneCallApi() {
+        const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=880ffa59f6aeed6d569e7450459fec7e');
 
-    const oneCallData = await response.json();
-    console.log(oneCallData);
-}
+        const oneCallData = await response.json();
+        console.log(oneCallData);
+    }
 
-oneCallApi();
+    async function getCoordinates() {
+        const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=880ffa59f6aeed6d569e7450459fec7e');
+
+        const coordinateData = await response.json();
+        console.log(coordinateData);
+    }
+
+    console.log(navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        console.log(latitude, longitude)
+    }));
+
+    oneCallApi();
+    getCoordinates();
+})();
+
