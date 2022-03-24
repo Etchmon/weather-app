ea1e693cc089196daa323a442b721fa2ec3d1560
James Etchells <etch.rush@gmail.com>
2022-03-23 18:17:54 -0700

    Add weekly cards and current temp div into position and styled

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const Handler = (() => {
     async function oneCallApi(lat, long) {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,daily&appid=880ffa59f6aeed6d569e7450459fec7e`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=880ffa59f6aeed6d569e7450459fec7e`);
 
         const oneCallData = await response.json();
-        console.log(oneCallData);
 
         return oneCallData;
     }
@@ -12,7 +11,6 @@ const Handler = (() => {
         const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=880ffa59f6aeed6d569e7450459fec7e`);
 
         const coordinateData = await response.json();
-        console.log(coordinateData);
 
         return coordinateData;
     }
@@ -29,10 +27,11 @@ const Display = (() => {
 
     const inputForm = () => {
         const dom = document.querySelector('.content');
-        const container = document.createElement('div');
+        const container = document.createElement('nav');
         const input = document.createElement('input');
         const btn = document.createElement('button');
 
+        container.setAttribute('class', 'search');
         btn.innerHTML = 'search';
         btn.onclick = () => getInput();
         Object.assign(input, {
@@ -42,28 +41,176 @@ const Display = (() => {
 
         async function getInput() {
             const data = await Handler.getCoordinates(input.value);
-            console.log(data);
             const lat = data[0].lat.toFixed(2);
             const long = data[0].lon.toFixed(2);
-
-            console.log(lat, long);
-
             const dataApi = await Handler.oneCallApi(lat, long);
 
-            console.log(dataApi);
-
+            DisplayController.setHeader(`${data[0].name}, ${data[0].state}`);
+            DisplayController.setDash(dataApi);
         };
 
         container.appendChild(input);
         container.appendChild(btn);
         dom.appendChild(container);
-
-        return getInput;
     };
+
+    const dash = () => {
+        const dom = document.querySelector('.content');
+        const section = document.createElement('section');
+        const header = document.createElement('h1');
+        const container = document.createElement('div');
+
+        container.setAttribute('class', 'grid');
+
+        section.appendChild(header);
+        section.appendChild(container)
+        dom.appendChild(section);
+    }
+
+    const currentTemp = () => {
+        const grid = document.querySelector('.grid');
+        const element = document.createElement('div');
+        const box = document.createElement('div');
+        const temp = document.createElement('span');
+        const feelsLike = document.createElement('span');
+        const humidity = document.createElement('span');
+        const condition = document.createElement('img');
+        const wind = document.createElement('span');
+        const riseSet = document.createElement('span');
+
+        Object.assign(element, {
+            className: 'current',
+        });
+        Object.assign(box, {
+            className: 'current-box',
+        });
+        Object.assign(temp, {
+            className: 'current-temp',
+        });
+        Object.assign(feelsLike, {
+            className: 'current-feels-like',
+        });
+        Object.assign(humidity, {
+            className: 'current-humidity',
+        });
+        Object.assign(wind, {
+            className: 'current-wind',
+        });
+        Object.assign(riseSet, {
+            className: 'current-rise',
+        });
+        Object.assign(condition, {
+            className: 'current-condition',
+        });
+
+        box.appendChild(feelsLike);
+        box.appendChild(humidity);
+        box.appendChild(wind);
+        box.appendChild(riseSet);
+
+        element.appendChild(condition);
+        element.appendChild(temp);
+        element.appendChild(box);
+
+        grid.appendChild(element);
+    };
+
+    const weekTemps = () => {
+        const grid = document.querySelector('.grid');
+        const element = document.createElement('div');
+
+        element.setAttribute('class', 'week-temps');
+
+        grid.appendChild(element);
+    }
+
+    const weekTempCard = () => {
+        const card = document.createElement('div');
+        const day = document.createElement('h2');
+        const img = document.createElement('img');
+        const temp = document.createElement('span');
+        const condition = document.createElement('span');
+
+        card.setAttribute('class', 'week-card');
+        day.setAttribute('class', 'week-day');
+        img.setAttribute('class', 'week-img');
+        temp.setAttribute('class', 'week-temp');
+        condition.setAttribute('class', 'week-condition');
+
+        card.appendChild(day);
+        card.appendChild(img);
+        card.appendChild(temp);
+        card.appendChild(condition);
+
+        return card;
+
+    }
 
     const initiateDom = (() => {
         inputForm();
+        dash();
+        currentTemp();
+        weekTemps();
     })();
 
-    return { inputForm }
+    return { inputForm, currentTemp, weekTempCard }
+})();
+
+const DisplayController = (() => {
+
+    const setHeader = (newHeader) => {
+        const header = document.querySelector('section h1');
+
+        header.innerHTML = newHeader;
+    };
+
+    const setDash = (data) => {
+        console.log(data);
+        console.log(data.current);
+        setCurrentTemp(data);
+        setWeekTemps(data);
+    }
+
+    const setWeekTemps = (data) => {
+        console.log(data)
+        data.daily.forEach(element => {
+            const container = document.querySelector('.week-temps');
+            const card = Display.weekTempCard();
+
+            card.querySelector('.week-day').innerHTML = getHumanDay(element.dt);
+            card.querySelector('.week-img').src = `http://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`;
+            card.querySelector('.week-temp').innerHTML = element.temp.day.toFixed(0) + '°F';
+            card.querySelector('.week-condition').innerHTML = element.weather[0].main;
+
+            container.appendChild(card);
+        });
+    }
+
+    const setCurrentTemp = (data) => {
+        const temp = document.querySelector('.current-temp');
+        const feelsLike = document.querySelector('.current-feels-like');
+        const condition = document.querySelector('.current-condition');
+        const riseSet = document.querySelector('.current-rise');
+        const humidity = document.querySelector('.current-humidity');
+        const wind = document.querySelector('.current-wind');
+
+        temp.innerHTML = data.current.temp.toFixed(0);
+        feelsLike.innerHTML = 'feels like ' + data.current.feels_like.toFixed(0) + '°F';
+        condition.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png`;
+        riseSet.innerHTML = `Sunrise: ${data.current.sunrise} Sunset: ${data.current.sunset}`;
+        humidity.innerHTML = 'humidity: ' + data.current.humidity + '%';
+        wind.innerHTML = 'wind: ' + data.current.wind_speed + ' mph';
+    }
+
+    const getHumanDay = (time) => {
+        const unixTime = time;
+        const miliseconds = time * 1000;
+        const dateObject = new Date(miliseconds);
+
+        const humanDate = dateObject.toLocaleString("en-US", { weekday: "long" })
+
+        return humanDate;
+    }
+
+    return { setHeader, setDash };
 })();
